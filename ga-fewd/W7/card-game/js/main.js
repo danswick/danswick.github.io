2e572faece5296fce9bdb99b5d3cd64ce82fecec
Dan Swick <dan.swick@gmail.com>
2015-02-25 23:04:26 -0600

    so many class things

--- ga-fewd/W7/card-game/js/main.js
+++ ga-fewd/W7/card-game/js/main.js
@@ -1,17 +1,23 @@
+// $(document).on('click', ':submit', function( e ) {
+// 	e.preventDefault;
+// 	numCards = parseInt($('.text-input').val());
+// });
+
 (function () {
 
 }());
-	
-// store list of cards 
-var cards = ['king', 'queen', 'king', 'queen'];
-
-var cardMap = {
-	king: 'img/King.png',
-	queen: 'img/Queen.png'
-};
 
 // store variable to denote the number of cards in play
-var numCards = 4;
+var numCards = prompt("How many cards?", "Even numbers only, please!");
+	
+// store list of cards 
+var allCards = ['kingC', 'queenC', 'jackC', 'tenC', 'nineC', 'eightC', 'sevenC', 'sixC', 'fiveC', 'fourC', 'threeC', 'twoC', 'aceC', 'kingH', 'queenH', 'jackH', 'tenH', 'nineH', 'eightH', 'sevenH', 'sixH', 'fiveH', 'fourH', 'threeH', 'twoH', 'aceH']
+
+var cardsProto = allCards.slice(0, (numCards/2));
+
+var cards = cardsProto.concat(cardsProto);
+
+
 
 for(i=0; i < numCards; i++){
 		$('#card-container').append("<div class='card'></div>");
@@ -47,15 +53,15 @@ var card1;
 $('.card').on('click', function(){
 
 	if($(this).hasClass('flipped') && !$(this).hasClass('matched')) {
-		$('.flipped').removeClass('flipped').css("background-image", 'url("img/back-of-card.png")');
+		$('.flipped').removeClass('flipped').css("background", 'url("img/back-of-card.png")');
 	} else if($('.flipped').length === 0 && !$(this).hasClass('matched')) {
 		var cardType = cards[$(this).index()];
-		$(this).addClass('flipped').css("background-image", "url(" + cardMap[cardType] + ")");
+		$(this).addClass('flipped').css("background", cardMap[cardType] );
 		card1 = $(this).attr('class');
 	} else if($('.flipped').length === 1 && !$(this).hasClass('flipped') && !$(this).hasClass('matched')) {
 		// add the 'flipped' class to the newly-flipped card
 		var cardType = cards[$(this).index()];
-		$(this).addClass('flipped').css("background-image", "url(" + cardMap[cardType] + ")");
+		$(this).addClass('flipped').css("background", cardMap[cardType] );
 		// if this card's class is === the other flipped card's class...
 		// guhhhhh returning -1. Position in returned array is fixed in the order of
 		// the divs, so can't use this method. 
@@ -69,13 +75,14 @@ $('.card').on('click', function(){
 		if($(this).hasClass('matched')) {
 			//exit
 		} else {
-			// otherwise, remove the flipped class from all cards
-			$('.card').removeClass('flipped matched');
-			$('.card').css("background-image", 'url("img/back-of-card.png")');
+			// otherwise, remove the flipped class from all cards EXCEPT MATCHED
+			$('.card').removeClass('flipped');
+			$('.card').not('.matched').css("background", 'url("img/back-of-card.png")');
 			$(this).addClass('flipped');
 
 			var cardType = cards[$(this).index()];
-			$(this).css("background-image", "url(" + cardMap[cardType] + ")");
+			$(this).css("background", cardMap[cardType] );
+			card1 = $(this).attr('class'); 
 		}
 	}
 
@@ -84,22 +91,36 @@ $('.card').on('click', function(){
 
 
 $("button").on('click', function() {
-	$('.card').removeClass().addClass('card').css("background-image", 'url("img/back-of-card.png")');
+	$('.card').removeClass().addClass('card').css("background", 'url("img/back-of-card.png")');
 	cardSort();
 });
 
-
-/*$('.card').hover(function() {
-	if($("#cheaty").prop('checked') && 
-		$('.card').attr('class') != ('flipped' || 'matched') &&
-		$(this).attr("class") === $($('.flipped')[$('.flipped').length - 2]).attr("class")) {
-			$(this).addClass('cheat');
-		}
-		}, function() {
-			$(this).removeClass('cheat');
-});*/
-
-
-
-// when there is a match, add "matched" class to each matched card and apply a style
-// that indicates the cards are out of play. 
+var cardMap = {
+	kingC: "url('img/classic-playing-cards.png') -876px 0",
+	queenC: "url('img/classic-playing-cards.png') -803px 0",
+	jackC: "url('img/classic-playing-cards.png') -730px 0",
+	tenC: "url('img/classic-playing-cards.png') -657px 0",
+	nineC: "url('img/classic-playing-cards.png') -584px 0",
+	eightC: "url('img/classic-playing-cards.png') -511px 0",
+	sevenC: "url('img/classic-playing-cards.png') -438px 0",
+	sixC: "url('img/classic-playing-cards.png') -365px 0",
+	fiveC: "url('img/classic-playing-cards.png') -292px 0",
+	fourC: "url('img/classic-playing-cards.png') -219px 0",
+	threeC: "url('img/classic-playing-cards.png') -146px 0",
+	twoC: "url('img/classic-playing-cards.png') -73px 0",
+	aceC: "url('img/classic-playing-cards.png') 0 0",
+	
+	kingH: "url('img/classic-playing-cards.png') -876px -98px",
+	queenH: "url('img/classic-playing-cards.png') -803px -98px",
+	jackH: "url('img/classic-playing-cards.png') -730px -98px",
+	tenH: "url('img/classic-playing-cards.png') -657px -98px",
+	nineH: "url('img/classic-playing-cards.png') -584px -98px",
+	eightH: "url('img/classic-playing-cards.png') -511px -98px",
+	sevenH: "url('img/classic-playing-cards.png') -438px -98px",
+	sixH: "url('img/classic-playing-cards.png') -365px -98px",
+	fiveH: "url('img/classic-playing-cards.png') -292px -98px",
+	fourH: "url('img/classic-playing-cards.png') -219px -98px",
+	threeH: "url('img/classic-playing-cards.png') -146px -98px",
+	twoH: "url('img/classic-playing-cards.png') -73px -98px",
+	aceH: "url('img/classic-playing-cards.png') 0 -98px"
+};
