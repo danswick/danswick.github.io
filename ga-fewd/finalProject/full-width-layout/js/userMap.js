7a41ca48cbc40d0e3222c96dd8cf0172e275cb6f
Dan Swick <dan.swick@gmail.com>
2015-03-18 14:24:45 -0500

    js changes

--- ga-fewd/finalProject/full-width-layout/js/userMap.js
+++ ga-fewd/finalProject/full-width-layout/js/userMap.js
@@ -3,20 +3,44 @@ var mapTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/danswick.lff6ij2d/{z
       maxZoom: 17,
     });
 
-var regionStyle = {
-	"color": "green",
-	"opacity": 0.5
 var givTiles = L.tileLayer('http://tilehut-danswick.rhcloud.com/GIV_subset/{z}/{x}/{y}.png', {
 	maxZoom: 17,
 });
 
+var ecosystemStyles = {
+	woodland: { "color": "#a6611a",	"opacity": 0.5 },
+	prairie: { "color": "#dfc27d", "opacity": 0.5 },
+	wetland: { "color": "#80cdc1", "opacity": 0.5 },
+	stream: { "color": "#018571", "opacity": 0.5  },
+	econet: { "color": "darkgreen",	"opacity": 0.5 }
 };
 
-$.getJSON("CW_region.geojson", function(regionData){
+var map = L.map('leaflet-map', {
+	center: [41.902, -89.453],
+	zoom: 8
+});
+
+mapTiles.addTo(map);
+givTiles.addTo(map);
+
+$('.secondary-nav a').on('click', function(){
+	$('.leaflet-zoom-animated g').remove(); // ugh, this is terrible, but i can't think of a better way to remove the existing layer before adding the new one
+	var regionStyle = ecosystemStyles[$(this).data('tab')];
+	$.getJSON("CW_region.geojson", function(regionData){
+		var geojson = L.geoJson(regionData, {
+			onEachFeature: function (feature, layer) {
+				layer.bindPopup(feature.properties.Name);
+			}
+		});
+		geojson.setStyle(regionStyle);
+		geojson.addTo(map);
+	});
+});
+
+/*$.getJSON("CW_region.geojson", function(regionData){
 	var geojson = L.geoJson(regionData, {
 		onEachFeature: function (feature, layer) {
 			layer.bindPopup(feature.properties.Name);
-		style: regionStyle
 		}
 	});
 	
@@ -26,5 +50,6 @@ $.getJSON("CW_region.geojson", function(regionData){
 	});
 
 	mapTiles.addTo(map);
+	geojson.setStyle(regionStyle);
 	geojson.addTo(map);
-});
+});*/
