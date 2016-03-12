3d1d012eb842b364f94babc34d76f9c5ba3a9ed0
danswick <dan.swick@gmail.com>
2016-03-11 18:58:52 -0800

    update some styles, add treasures

---
+++ adventure-map/waypoints.js
@@ -0,0 +1,75 @@
+var waypoints = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bunker",
+        "description": "You meet a dog..."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.53694415092468,
+          37.83203941565131
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Storage room",
+        "description": "You find an item..."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.53561913967133,
+          37.832730024277396
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Overlook",
+        "description": "You can see everything from here...<br><button onclick='satelliteHint()'>Take a look</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.52974510192871,
+          37.82154814863567
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Trail intersection",
+        "description": "You cross paths with another traveller..."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.51800775527954,
+          37.83525937613405
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Start!",
+        "description": "You can see everything from here...<br><button onclick='satelliteHint()'>Take a look</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.54311323165894,
+          37.839072305476435
+        ]
+      }
+    }
+  ]
+};
