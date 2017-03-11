e8350f776aa5baad0077787b07a269eade82c3df
Dan Swick <dan.swick@gmail.com>
2017-03-10 17:13:04 -0800

    Update main.js

--- fuckyeahfriend/js/main.js
+++ fuckyeahfriend/js/main.js
@@ -153,7 +153,7 @@ function autoPlay() {
 		$playButton.toggleClass('.autoplay-on');
 		window.clearInterval(intervalVar);
 	} else {
-		window.setInterval(function(){buildPage(sheetData)}, 35000);
+		window.setInterval(function(){buildPage(sheetData)}, 25000);
 		$playButton.toggleClass('autoplay-on');
 	}
 }
